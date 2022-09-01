23125a4a1b630ee24bf755fbb230d4094f3a2466
dylan <dylan-reinhard@bluewin.ch>
2022-09-01 19:43:19 +0200

    All files

---
+++ src/main/java/net/epicowl55/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.epicowl55.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
